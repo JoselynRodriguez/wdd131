35a1618433ecdb6f64b62c3c52ed6f584d9c023e
JoselynRodriguez <jrosmerirodriguezr@gmail.com>
2025-07-19 23:17:06 -0500

    changes last week2

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,11 @@ document.getElementById("currentyear").textContent = year;
 
 const lastModDate = document.lastModified;
 document.getElementById("lastModified").textContent = `Last Modification: ${lastModDate}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
