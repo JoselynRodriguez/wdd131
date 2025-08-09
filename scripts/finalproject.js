6f8611b0c2c2240b1b711ecc2a12dc85963b02cf
JoselynRodriguez <jrosmerirodriguezr@gmail.com>
2025-08-09 18:43:36 -0500

    update

--- scripts/finalproject.js
+++ scripts/finalproject.js
@@ -1,5 +1,15 @@
-const year = new Date().getFullYear();
-document.getElementById("currentyear").textContent = year;
 
-const lastModDate = document.lastModified;
-document.getElementById("lastModified").textContent = `Last Modification: ${lastModDate}`;
+const yearSpan = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+const lastModified = document.getElementById("lastModified");
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
+const hamButton = document.getElementById("hambutton");
+const navMenu = document.querySelector("nav.windows");
+
+hamButton.addEventListener("click", () => {
+  navMenu.classList.toggle("open");
+  hamButton.classList.toggle("open");
+});
